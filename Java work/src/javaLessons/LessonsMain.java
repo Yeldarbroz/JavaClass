c6dc57e3cfd71db3096fafba6154fefd3b411dfe
Yeldarbroz <Yeldarb.roz@gmail.com>
2015-03-13 14:34:06 -0400

    added main hub for all java work

--- Java work/src/javaLessons/LessonsMain.java
+++ Java work/src/javaLessons/LessonsMain.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class LessonsMain {
 
-	public static void main(String args[]){
+	public void run(){
 		
 		Scanner userInput = new Scanner(System.in);
 		
-		//boolean doneLearning = false;
+		boolean doneLearning = false;
 			
-		//while(doneLearning == false){
+		while(doneLearning == false){
 				
 			System.out.println("What would you like to learn now? (type done to finish learning)");
 				
@@ -26,15 +26,15 @@ public class LessonsMain {
 				
 			case "if statements": IfLesson.run();
 			break;
-			/*
-			case "done": //doneLearning = true;
+			
+			case "done": doneLearning = true;
 			break;
-			*/
+			
 			default : System.out.println("Sorry, I'm not sure what you want to learn. Could you run the program again and try another word?");
 				
 			}
 		
-		//}
+		}
 		
 	}
 	
