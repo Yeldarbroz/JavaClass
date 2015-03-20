3d163bcf2bf3bedd05ef7c4f2129d3f78712581b
Yeldarbroz <Yeldarb.roz@gmail.com>
2015-03-20 13:51:33 -0400

    Added to lessons; made hub, command prompt style

--- Java work/src/javaLessons/LessonsMain.java
+++ Java work/src/javaLessons/LessonsMain.java
@@ -4,38 +4,47 @@ import java.util.Scanner;
 
 public class LessonsMain {
 
-	public void run(){
-		
-		Scanner userInput = new Scanner(System.in);
-		
-		boolean doneLearning = false;
-			
-		while(doneLearning == false){
-				
-			System.out.println("What would you like to learn now? (type done to finish learning)");
-				
-			String lesson = userInput.next();
-			
-			switch (lesson){
-				
-			case "if": IfLesson.run();
-			break;
-				
-			case "if statement": IfLesson.run();
-			break;
-				
-			case "if statements": IfLesson.run();
-			break;
-			
-			case "done": doneLearning = true;
-			break;
-			
-			default : System.out.println("Sorry, I'm not sure what you want to learn. Could you run the program again and try another word?");
-				
-			}
-		
-		}
-		
-	}
-	
+        public static void run(){
+
+                Scanner userInput = new Scanner(System.in);
+
+                boolean doneLearning = false;
+
+                while(doneLearning == false){
+
+                        System.out.println("What would you like to learn now? (type done to finish learning)");
+
+                        String lesson = userInput.next();
+
+                        switch (lesson){
+
+                        case "if": IfLesson.run();
+                        break;
+
+                        case "if statement": IfLesson.run();
+                        break;
+
+                        case "if statements": IfLesson.run();
+                        break;
+
+                        case "while": WhileLesson.run();
+                        break;
+
+                        case "while loop": WhileLesson.run();
+                        break;
+
+                        case "while loops": WhileLesson.run();
+                        break;
+
+                        case "done": doneLearning = true;
+                        break;
+
+                        default : System.out.println("Sorry, I'm not sure what you want to learn. Could you run the program again and try another word?");
+
+                        }
+
+                }
+
+        }
+
 }
--- Java work/src/main/Main.java
+++ Java work/src/main/Main.java
@@ -1,11 +1,35 @@
 package main;
 
+import java.util.*;
 import javaLessons.LessonsMain;
 
 public class Main {
 
-	public static void main(String args[]){
-		
-	}
-	
+        public static void main(String args[]){
+
+                Scanner userInput = new Scanner(System.in);
+
+                String luanchApplication = userInput.next();
+
+                switch(luanchApplication){
+
+                case "lessons": LessonsMain.run();
+                break;
+                /*
+                case "": ;
+                break;
+
+                case "": ;
+                break;
+
+                case "": ;
+                break;
+
+                case "": ;
+                break;
+                */
+                }
+
+        }
+
 }
